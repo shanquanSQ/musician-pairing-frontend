3d93b1cf56d2d52aabee97d131aaab04ade51028
shanquanSQ <thiashanquan93@gmail.com>
2023-11-14 20:56:57 +0800

    frontend updates, sockets has been commented out but probably need to npm i if the package is missing

--- src/Pages/SingleJamRoomPage.js
+++ src/Pages/SingleJamRoomPage.js
@@ -5,11 +5,40 @@ import axios from "axios";
 // Import Components
 import { SpeechBubble } from "../Components/SpeechBubble/SpeechBubble";
 
-export const SingleJamRoomPage = () => {
-  const [roomData, setRoomData] = useState(null);
-  const { chatId } = useParams();
+// Import Sockets
+// import { io } from "socket.io-client"; // io is a function to call an individual socket. the package for frontend(client side) is npm i socket.io-client
+// const socket = io(`http://localhost:8080`);
 
-  const userId = "1";
+export const SingleJamRoomPage = () => {
+  const [roomData, setRoomData] = useState("");
+  const [userMessage, setUserMessage] = useState({});
+
+  const { chatroomId } = useParams();
+
+  const handleTextChange = (ev) => {
+    let name = ev.target.name;
+    let value = ev.target.value;
+
+    setUserMessage((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  const handleSubmitMessage = (ev) => {
+    ev.preventDefault();
+    console.log(userMessage);
+    // socket.emit("send-message", userMessage, chatroomId);
+  };
+
+  // The URL of the backend goes into the socket
+  // const socket = io(`${process.env.REACT_APP_BACKEND_URL}`); // Doesnt work, Express server is on 8000. So we'll use 8080 for sockets.
+  // socket.on("connect", () => {
+  //   console.log(`You connected with id: ${socket.id}`);
+  // });
+
+  // socket.on("receive-message", (message) => {
+  //   console.log(message);
+  // });
 
   const dummyData = [
     {
@@ -116,7 +145,7 @@ export const SingleJamRoomPage = () => {
     <>
       <div className="flex flex-row justify-center h-[100dvh] pt-[2em] pb-[4em] px-[2em] ">
         <div className="flex flex-col w-full lg:w-[30%] justify-between overflow-x-hidden overflow-y-auto">
-          <div className="flex flex-col pt-[2em] mb-[-10em]">
+          <div className="flex flex-col pt-[2em] mb-[-10em] h-[100%]">
             <h1 className="font-bold text-txtcolor-primary text-[1.5rem] pb-[1em] text-left">
               JAM ROOM TITLE SHOULD GO HERE
             </h1>
@@ -132,11 +161,12 @@ export const SingleJamRoomPage = () => {
             </button>
             <br /> */}
 
-            <div className="pr-[2em]">
+            {/* Sorting message left and right by user logged in */}
+            <div className="pr-[1.5em] h-[80%] mb-[1em] border-b-[1px] border-slate-300 overflow-y-auto">
               {roomData &&
                 roomData.map((elementdata, index) => {
-                  if (elementdata.author_id == userId) {
-                    console.log("true");
+                  if (elementdata.author_id === 1) {
+                    // console.log("true");
                     return (
                       <>
                         <div
@@ -165,6 +195,28 @@ export const SingleJamRoomPage = () => {
                       </>
                     );
                 })}
+            </div>
+
+            <div className="flex flex-col justify-end h-[20%] gap-[1em] pr-[1.5em] text-right">
+              <div>
+                <textarea
+                  type="text"
+                  name="message"
+                  onChange={handleTextChange}
+                  value={userMessage.message}
+                  autoComplete="off"
+                  placeholder="message"
+                  className="bg-white w-[100%] h-[100%] p-[1em] rounded-md border-slate-400 border-[1px] focus:outline-none"
+                />
+              </div>
+              <div>
+                <button
+                  onClick={handleSubmitMessage}
+                  className="bg-fill-secondary px-[1em] text-white font-semibold rounded-md active:outline-none scale-100 transition-all active:scale-95"
+                >
+                  SEND
+                </button>
+              </div>
             </div>
           </div>
         </div>
